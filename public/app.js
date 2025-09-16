8fe6fde5b56ca16f4b60623f584e2b8bbe951a01
DeeKayDKdk <76975619+DeeKayDKdk@users.noreply.github.com>
2025-09-15 19:57:58 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -101,7 +101,7 @@ with a real human holding you accountable.
 <a class="btn btn-ghost" href="/why.html">See Why It Works</a>
 <a class="btn btn-ghost" href="/demoralization.html"><b>Demoralization Index</b></a>
 
-<!-- Login button (will be swapped to “Open Platinum Dashboard” by Supabase script) -->
+<!-- Login button (JS on index.html will swap text + link if the user is Platinum) -->
 <a id="loginOrDash" class="btn btn-secondary" href="/login.html">Log in</a>
 </div>
 </section>
